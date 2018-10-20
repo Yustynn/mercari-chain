a72e62b60d44f1b42eefbba026381bd34381fe75
Christabella Irwanto <christabella.irwanto@gmail.com>
2018-10-20 15:28:47 +0200

    Add a core_score module to update core scores

---
+++ projectx/core_score.py
@@ -0,0 +1,7 @@
+import sqlite3
+
+def update_core_score_with_review(conn):
+    # Read all reviews from DB
+    # Compute new core score
+    # Write new core score to DB
+    pass
--- projectx/server.py
+++ projectx/server.py
@@ -1,11 +1,12 @@
 '''Trust compuation server'''
 from flask import Flask, request
 from graph import ReputationGraph
+from core_score import update_core_score_with_review
 import sqlite3
 
 app = Flask(__name__)
 graph = ReputationGraph()
-conn = sqlite3.connect('example.db')
+conn = sqlite3.connect('db.db')
 
 # POST methods
 @app.route('/add_new_user/<int:user_id>')
@@ -19,7 +20,10 @@ def add_review(user_id):
     '''Add a new review to db and call graph.update'''
     rating = request.form['rating']
     content = request.form['content']
-    #... this.update()
+    # Write to db
+    # Update core score with review
+    update_core_score_with_review(conn)
+    #... graph.update()
     pass
 
 # GET methods
