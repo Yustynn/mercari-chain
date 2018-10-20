d2e3a8a9bda98fd0405f7aa0b8f25da86281dd43
Christabella Irwanto <christabella.irwanto@gmail.com>
2018-10-20 14:35:42 +0200

    Skeleton classes for project x server and graph

---
+++ projectx/graph.py
@@ -0,0 +1,18 @@
+'''Graph algorithm'''
+import networkx
+
+class ReputationGraph():
+    def __init__(self):
+        self.nodes = []
+        self.edges = []
+
+    def update(self):
+        '''Recomputes graph weights'''
+        # read all friendships and reviews in from db
+        # compute with score algorithm
+        pass
+
+    def get_score_confidence(self, userid):
+        '''Get the value of a given node'''
+        # return (score, confidence)
+        pass
