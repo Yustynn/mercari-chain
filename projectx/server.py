c80ee631a64c9c8c4287923dcc5e9bd5c32ca48d
Christabella Irwanto <christabella.irwanto@gmail.com>
2018-10-20 14:35:42 +0200

    Super simple skeleton classes for project x server and graph

---
+++ projectx/server.py
@@ -0,0 +1,24 @@
+'''Trust compuation server'''
+from flask import Flask
+from graph import ReputationGraph
+import sqlite3
+
+app = Flask(__name__)
+graph = ReputationGraph()
+
+# POST methods
+@app.route('/add_new_user')
+def add_new_user(self, user_id, friends):
+    '''Add a new user to db and call graph.update'''
+    pass
+
+@app.route('/add_review/{user_id}')
+def add_review(self, userid, rating):
+    '''Add a new review to db and call graph.update'''
+    #... this.update()
+    pass
+
+# GET methods
+@app.route('')
+def get_score_confidence(fb_user_id, platform):
+    pass
